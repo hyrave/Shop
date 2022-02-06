01d539c331f3eab90e738c728143366de83becdc
hyrave <hyrave@gmail.com>
2022-02-06 16:01:46 -0400

    re starting learning on this lesson

---
+++ app.js
@@ -0,0 +1,18 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+
+const adminRoutes = require('./routes/admin');
+const shopRoutes = require('./routes/shop');
+
+const app = express();
+
+app.use(bodyParser.urlencoded({extended: false}));
+
+app.use('/admin',adminRoutes);
+app.use(shopRoutes);
+
+app.use((req,res,next) => {  
+  res.status(404).end('<h1>Pange not found</h1>');  
+});
+
+app.listen(3000);
