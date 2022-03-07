ac2f9ffe61f5db41d17e1e713f827d754d0522b4
hyrave <hyrave@gmail.com>
2022-03-06 21:41:33 -0400

    done lesson 130

--- app.js
+++ app.js
@@ -9,11 +9,11 @@ const app = express();
 app.set('view engine','ejs');
 app.set('views','views');
 
+app.use(express.static(path.join(__dirname,'public')));
+app.use(bodyParser.urlencoded({extended: false}));
+
 const adminRoutes = require('./routes/admin');
 const shopRoutes = require('./routes/shop');
-
-app.use(bodyParser.urlencoded({extended: false}));
-app.use(express.static(path.join(__dirname,'public')));
 
 app.use('/admin',adminRoutes);
 app.use(shopRoutes);
