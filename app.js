48d8a9dcdf2d3ac8d309d328d2763cabd41ee271
hyrave <hyrave@gmail.com>
2022-10-25 23:25:21 -0300

    done lesson 153

--- app.js
+++ app.js
@@ -3,30 +3,29 @@ const express = require('express');
 const bodyParser = require('body-parser');
 
 const errorController = require('./controllers/error');
-const db = require('./util/database');
+const sequelize = require('./util/database');
 
 const app = express();
 
 app.set('view engine','ejs');
 app.set('views','views');
 
-app.use(express.static(path.join(__dirname,'public')));
-app.use(bodyParser.urlencoded({extended: false}));
-
 const adminRoutes = require('./routes/admin');
 const shopRoutes = require('./routes/shop');
 
-/*
-db.execute('SELECT * FROM products')
-   .then(result => {
-      console.log(result[0],result[1]);
-   })
-   .catch(err => {
-      console.log(err);
-   });
-*/
+app.use(express.static(path.join(__dirname,'public')));
+app.use(bodyParser.urlencoded({extended: false}));
+
 app.use('/admin',adminRoutes);
 app.use(shopRoutes);
 app.use(errorController.get404);
 
-app.listen(3000);
+sequelize
+.sync()
+.then(result => {
+  //console.log(result);
+  app.listen(3000);
+}).catch(err => {
+  console.log(err);
+});
+
